89c292be4ece36353b36794de7424d92f7e11371
Deadchu-R <93131102+Deadchu-R@users.noreply.github.com>
2022-04-18 12:17:09 +0300

    sdv
    sdfv

---
+++ Portal Project/Assets/Scripts/FirstScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstScript : MonoBehaviour
+{
+    public float speedZ = 5;
+    public float speedX = 5;
+    public Vector3 direction;
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        transform.position = transform.position + direction;
+    }
+
+    
+
+}
---
+++ Portal Project/Assets/Scripts/MovingPlatforms.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatforms : MonoBehaviour
+{
+    public float speed;
+    public Vector3 point1;
+    public Vector3 point2;
+    private Vector3 direction;
+    private bool isGoingToPoint2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        direction = (point2 - point1).normalized;  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
